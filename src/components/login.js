1c70c2d8f04843d9eb51bce04333eb9425c643c4
Utkarsh <utkarshbhimte95@gmail.com>
2017-02-01 16:10:58 +0530

    done with fb login integration

---
+++ src/components/login.js
@@ -0,0 +1,84 @@
+/**
+ * Created by utkarsh on 31/01/17.
+ */
+import React, {
+    Component
+} from 'react';
+import base from '../base';
+
+class Login extends Component {
+    constructor(){
+        super();
+
+        this.authenticate = this.authenticate.bind(this);
+        this.authHandler = this.authHandler.bind(this);
+        this.addCustomUserData = this.addCustomUserData.bind(this);
+    }
+
+    addCustomUserData(e){
+        e.preventDefault();
+
+        this.props.updateRegistrationData( parseInt(this.admission_year.value), 'admission_year');
+        this.props.updateRegistrationData( this.branch.value, 'branch');
+
+        // this.props.user.admission_year = this.admission_year;
+        // this.props.user.branch = this.branch;
+
+        this.registrationForm.reset();
+    }
+
+    authenticate(provider){
+        console.log(`trying to login with ${provider}`);
+
+        base.authWithOAuthPopup(provider, this.authHandler);
+    }
+
+    authHandler(err, authData){
+        // const user = {...this.props.user};
+        const fetchedData  = authData.user.providerData[0];
+        console.log('fetched Data',fetchedData);
+
+        this.props.updateRegistrationData('name', fetchedData.name);
+
+        console.log('props: ', this.props.user);
+
+        if(err){
+            console.log(err);
+        }
+    }
+
+    render() {
+
+            return(
+                <div>
+                    <button onClick={() => {this.authenticate('facebook')} }>Sign in with Facebook</button>
+                    <form onSubmit={(e) => {this.addCustomUserData(e)} }
+                          ref={(input) => this.registrationForm = input}>
+                        <input type="number" ref={(input) => this.admission_year = input}/>
+                        <select  ref={(input) => this.branch = input} >
+                            <option value="EEE">EEE</option>
+                            <option value="IS">IS</option>
+                            <option value="CS">CS</option>
+                            <option value="IT">IT</option>
+                            <option value="ECE">ECE</option>
+                            <option value="MECH">MECH</option>
+                            <option value="CIVIL">CIVIL</option>
+                        </select>
+                    </form>
+
+                    <div>
+                        <div>
+                            <h4>Your Data:</h4>
+                            {/*<img src={this.props.user.image} alt={this.props.user.name}/>*/}
+                            {/*<span>{this.props.user.name}</span>*/}
+                        </div>
+                    </div>
+
+                </div>
+
+            );
+
+    }
+}
+
+export default Login;
--- src/components/msgMain.js
+++ src/components/msgMain.js
@@ -4,10 +4,12 @@
 import React, {
     Component
 } from 'react';
+import base from '../base';
 
 import Header from './msgMain/header'
 import Navbar from './msgMain/navbar'
 import ContactTabs from './msgMain/contacts-tab'
+import Login from './login'
 
 let Contacts = {
     // 'ds3232hgvj23':{
@@ -46,30 +48,6 @@ let Contacts = {
         image: 'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/10422935_899719646746010_1535978486254929389_n.jpg?oh=820368eb43db24f55c014de6c5fd81eb&oe=59134704',
         branch: 'ECE',
         admission_year: 2015
-    },
-    'uiou232vf423o':{
-        name: "Saujanya Nagpal",
-        image: 'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/15894514_1889762547920534_3231168312415608458_n.jpg?oh=a0d458ca8d9833e408fd160304de97b0&oe=59497381',
-        branch: 'IS',
-        admission_year: 2014
-    },
-    'uiou2aqwnvj23o':{
-        name: "Anusha Acharya",
-        image: 'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/14900320_1404870672874743_663108496078802753_n.jpg?oh=980470111c3b89fd4b95f77e682aabcc&oe=591235EE',
-        branch: 'CS',
-        admission_year: 2015
-    },
-    'dsdfn9f4nzjki':{
-        name: 'Shubham Kanth',
-        image: 'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/16003222_1081489585293946_3265617616157899583_n.jpg?oh=76c2801aa6d63a916de451d60066deb4&oe=59126074',
-        branch: 'IS',
-        admission_year: 2014
-    },
-    'dsfjk74n3974h4h':{
-        name: 'Navneet Tirth',
-        image: 'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/10422935_899719646746010_1535978486254929389_n.jpg?oh=820368eb43db24f55c014de6c5fd81eb&oe=59134704',
-        branch: 'ECE',
-        admission_year: 2015
     }
 };
 
@@ -79,10 +57,32 @@ class MsgMain extends Component {
 
         this.state = {
             contacts : Contacts,
-            preview: Contacts[Object.keys(Contacts)[0]]
-        }
+            preview: Contacts[Object.keys(Contacts)[0]],
+            uid: null,
+            user: {
+                displayName: null,
+                email: null,
+                photoURL: null,
+                admission_year: null,
+                branch: null,
+                fbUid: null
+            }
+        };
 
         this.updatePreview = this.updatePreview.bind(this);
+        this.updateRegistrationData = this.updateRegistrationData.bind(this);
+
+    }
+
+    componentWillMount(){
+          this.ref = base.syncState(`contacts` ,{
+                context : this,
+                state : 'contacts'
+          })
+        }
+
+    componentWillUnmount(){
+        base.removeBinding(this.ref);
     }
 
     updatePreview(key){
@@ -90,16 +90,45 @@ class MsgMain extends Component {
         this.setState({ preview });
     }
 
+
+    // This is for Authentication flow
+
+    updateRegistrationData( src, dest ){
+        let User = {...this.state.user};
+        console.log(User);
+        console.log('dest:', dest);
+        console.log('src:', src);
+        User[dest] = src;
+        console.log(User);
+        this.setState({user: User});
+    }
+
+
+    // Render Fucntion
+
     render() {
-        return (
-            <div className="contain-all">
-                <Header />
-                <Navbar />
-                <ContactTabs contacts={this.state.contacts}
-                             preview={this.state.preview}
-                             updatePreview={this.updatePreview}/>
-            </div>
-        );
+
+        if(this.state.uid === null){
+            return(
+                <div>
+                    <Login uid={this.state.uid}
+                           user={this.state.user}
+                           updateRegistrationData={this.updateRegistrationData}
+                    />
+                </div>
+            )
+        }else{
+            return (
+                <div className="contain-all">
+                    <Header />
+                    <Navbar />
+                    <ContactTabs contacts={this.state.contacts}
+                                 preview={this.state.preview}
+                                 updatePreview={this.updatePreview}/>
+                </div>
+            );
+        }
+
     }
 }
 
